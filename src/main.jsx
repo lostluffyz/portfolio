1520165ac51f6542cf6966e4a7924067123157d8
Luffyz <116288192+lostluffyz@users.noreply.github.com>
2025-01-15 16:26:16 +0000

    Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
